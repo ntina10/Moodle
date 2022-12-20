4222b40b6cdb87dab8f98d2ab55edaa35243efe9
ntina10 <74246485+ntina10@users.noreply.github.com>
2022-12-20 20:47:05 +0200

    added grid on the background

--- script.js
+++ script.js
@@ -1,54 +1,104 @@
+// the canvas 
+var w = screen.width + 1200;
+var h = document.body.scrollHeight; //+ screen.height;
 
+console.log('w', w, 'h', h);
+// grid step
+var step = 230; 
+var canvasElementId = 'grid';
+
+var canvas = document.getElementById(canvasElementId);
+// this is how you resize the canvas
+canvas.width  = w;
+canvas.height = h;
+
+var ctx = canvas.getContext('2d');
+
+// the render logic should be focusing on the rendering 
+var drawGrid = function(ctx, w, h, step) {
+    // set the color of the line
+    ctx.strokeStyle = '#BEBEBE';
+    ctx.lineWidth = 0.38;
+
+    ctx.beginPath(); 
+    for (var x=0;x<=w;x+=step) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, h);
+    }
+    
+    // the stroke will actually paint the current path 
+    ctx.stroke(); 
+    
+    ctx.beginPath(); 
+    for (var y=0;y<=h;y+=step) {
+            ctx.moveTo(0, y);
+            ctx.lineTo(w, y);
+    }
+
+    ctx.stroke(); 
+};
+
+drawGrid(ctx, w, h, step);
+
+// the moving of the canvas
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+console.log('height', height);
+
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var scrollArea = height - windowHeight;
+console.log('windowHeight', windowHeight, 'windowWidth', windowWidth);
+console.log('scrollArea', scrollArea);
+
+window.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || window.scrollTop;
+    var scrollPercent = scrollTop/scrollArea || 0;
+    
+    canvas.style.right = -scrollPercent*window.innerWidth*0.6 + 'px';
+    canvas.style.top = -1300 + scrollPercent*window.innerWidth*1.2 + 'px';
+    // square2.style.left = 800 - scrollPercent*window.innerWidth*0.6 + 'px';
+  });
+
+
+// the scrolling stuff of the horizontal scroller scrolling by vertical scroll
 // let scrollingWrapper = document.querySelector('.scrolling-wrapper');
 // let grid = document.querySelector('.grid-container');
 
-// scrollingWrapper.addEventListener('onmousewheel', () => {
-//     console.log('miaou');
-//     var positionY = window.scrollY;
-//     console.log(positionY);
-//     // scrollingWrapper.css("transform","translateX(-" + positionY + "%)");
-//     scrollingWrapper.style.transform = "translateX(-" + positionY / 100 + "%)";
-// });
+var controller = new ScrollMagic.Controller();
 
-// scrollingWrapper.onscroll = () => {
-//     $('.scrolling-wrapper').scrollLeft();
-// }
-// window.addEvent('scroll',function(e) {
-//     console.log('mememe');
-//     $('.scrolling-wrapper').scrollLeft(100);
+var scrollHorizontal = new TimelineLite()
+  scrollHorizontal.to("#scrolling", 1,   {x: "-33%"})	
+  .to("#scrolling", 1,   {x: "-66%"})
+  .to("#scrolling", 1,   {x: "-100%"})
+//   .to("#scrolling", 1, {x:'-300px'})
 
-// });
+var horizontalScroll = new ScrollMagic.Scene({
+      triggerElement: "#scrolling",
+      triggerHook: 'onLeave',
+      duration: "300%"
+    }).setPin("#scrolling").setTween(scrollHorizontal).addTo(controller);
 
-// var scroller = {};
-// scroller.e = document.querySelector('.scrolling-wrapper');
-// // document.getElementById("b");
+// var controller = new ScrollMagic.Controller();
 
-// if (scroller.e.addEventListener) {
-//     scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
-//     scroller.e.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-// } else scroller.e.attachEvent("onmousewheel", MouseWheelHandler);
+// var scrollHorizontal = new TimelineLite()
+//   scrollHorizontal.to("#scrolling-wrapper", 1,   {x: "-20%"})	
+//   .to("#scrolling-wrapper", 1,   {x: "-40%"})
+//   .to("#scrolling-wrapper", 1,   {x: "-60%"})
+//   .to("#scrolling-wrapper", 1,   {x: "-80%"})
+// //   .to("#scrolling", 1, {x:'-300px'})
 
-// function MouseWheelHandler(e) {
 
-//     // cross-browser wheel delta
-//     var e = window.event || e;
-//     var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
+// var horizontalScroll = new ScrollMagic.Scene({
+//       triggerElement: "#scrolling-wrapper",
+//       triggerHook: 'onLeave',
+//       duration: "400%"
+//     }).setPin("#scrolling-wrapper").setTween(scrollHorizontal).addTo(controller);
 
-//     var pst = $('.scrolling-wrapper').scrollLeft() + delta;
-
-//     if (pst < 0) {
-//         pst = 0;
-//     } else if (pst > $('.scrolling-wrapper').width()) {
-//         pst = $('.scrolling-wrapper').width();
-//     }
-
-//     $('.scrolling-wrapper').scrollLeft(pst);
-
-//     return false;
-// }
-
-// scrollingWrapper.attachEvent("onmousewheel", MouseWheelHandler);
-
+// FIRST PLAYER /////////////////////////////////////////////////
 const audio1 = document.getElementById('audio1');
 const btn1 = document.querySelector('.player1');
 let circularProgress = document.querySelector('.circular-progress');
